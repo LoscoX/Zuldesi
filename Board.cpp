7ea35f33ee116e449f279eabc6084005a1714e1c
DaveGit99 <120267533+DaveGit99@users.noreply.github.com>
2022-12-23 18:48:46 +0100

    FIrst version

---
+++ Board.cpp
@@ -0,0 +1,55 @@
+/*
+ * Board.cpp
+ *
+ *  Created on: 11 dic 2022
+ *      Author: david
+ */
+
+#include "Board.hpp"
+
+Board::Board(){
+	construct(0,0);
+}
+
+Board::Board(int height,int width){
+	construct(height,width);
+}
+
+void Board::construct(int height,int width){
+	int xMax,yMax;
+	getmaxyx(stdscr,yMax,xMax);
+	board_win = newwin(height,width, (yMax / 2) - (height / 2), (xMax / 2) - (width / 2));
+	wtimeout(board_win,50); //need this command for the movement of ALL characters (number of delay)
+	keypad(board_win,true);
+}
+
+void Board::initialize(){
+	clear();
+	addBorder();
+	refresh();
+}
+
+//void Board::addAt(int y, int x, char ch){
+	//mvwaddch(board_win,y,x,ch);
+//}
+
+char Board::getInput(){
+	return wgetch(board_win);
+}
+
+void Board::addBorder(){
+	box(board_win, 0, 0);
+}
+
+//void Board::add(Drawable drawable){
+	//addAt(drawable.getY(),drawable.getX(),drawable.geticon());
+//}
+
+void Board::clear(){
+	wclear(board_win);
+}
+
+void Board::refresh(){
+	wrefresh(board_win);
+}
+
---
+++ Player.cpp
@@ -0,0 +1,126 @@
+/*
+ * Player.cpp
+ *
+ *  Created on: 8 dic 2022
+ *      Author: david
+ */
+
+#include "Player.hpp"
+
+Player::Player(WINDOW * win, int y, int x, char c){
+	yLoc = y;
+	xLoc = x;
+	curwin = win;
+	getmaxyx(curwin,yMax,xMax);
+	keypad(curwin,true);
+	character = c;
+	life = 99;
+	cash = 0;
+	pwup = NULL;
+};
+
+Player::Player(){
+	yLoc = 0;
+	xLoc = 0;
+	curwin = newwin(0,0,0,0);
+	getmaxyx(curwin,yMax,xMax);
+	keypad(curwin,true);
+	character = '@';
+	life = 99;
+	cash = 0;
+	pwup = NULL;
+}
+
+void Player::initialize(){
+	display();
+}
+
+void Player::jump(int segno){ //jump dx,sx
+	int x = xLoc; //save xstart of jump
+	int y = yLoc; //save ystart of jump
+	int xv = x+3*segno; //xvertex of parabola
+	int yv = y - 9; //yvertex of parabola
+	int a = (y-yv)/((x-xv)*(x-xv));
+	for (int i=0;i<7;i++){ //7 = 2*3 + 1
+		mvwaddch(curwin, yLoc, xLoc,' '); //Delete previous character
+		xLoc = x + segno*i;
+		yLoc = a * (xLoc-xv) * (xLoc-xv) + yv; //parabola equation
+		if(xLoc < 1) xLoc = 1; //reach minimum
+		if(xLoc > xMax-2) xLoc = xMax - 2; //reach maximum
+		display();
+		wrefresh(curwin);
+        napms(40); //delay 40ms
+	}
+}
+
+void Player::mvupright(){  //move up
+	jump(1);
+}
+
+void Player::mvupleft(){ //move down
+	jump(-1);
+}
+void Player::mvleft(){ //move left
+	mvwaddch(curwin, yLoc, xLoc,' ');
+	xLoc = xLoc - 1;
+	if (xLoc < 1) xLoc = 1;
+}
+void Player::mvright(){ //move right
+	mvwaddch(curwin, yLoc, xLoc,' ');
+	xLoc = xLoc + 1;
+	if (xLoc > xMax-2) xLoc = xMax - 2;
+}
+int Player::getmv(){ //move the character by user
+	int choice = wgetch(curwin);
+	switch (choice){
+		case KEY_UP:
+			mvupright();
+			break;
+		case KEY_DOWN:
+			mvupleft();
+			break;
+		case KEY_LEFT:
+			mvleft();
+			break;
+		case KEY_RIGHT:
+			mvright();
+			break;
+		default:
+			break;
+	}
+	return choice;
+}
+
+void Player::display(){ //display the character
+	mvwaddch(curwin,yLoc,xLoc,character);
+}
+
+
+void Player::injury(){ //Injury
+	life = life - 1;
+}
+
+int Player::getx(){
+	return xLoc;
+}
+
+int Player::gety(){
+	return yLoc;
+}
+
+int Player::getlife(){
+	return life;
+}
+
+void Player::updatecash(int cash){ //update cash
+	this->cash = this->cash + cash;
+}
+
+powup Player::addpwup(powup h,int e,char icon){ //add powerup
+	powup tmp = new powerup;
+	tmp->val = e;
+	tmp->c = icon;
+	tmp->next = h;
+	return tmp;
+}
+
