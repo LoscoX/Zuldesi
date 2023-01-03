5ad8032e37f73aacc316c8105af53b47fb2a8414
Massimiliano Fardo <48696348+LoscoX@users.noreply.github.com>
2023-01-03 18:46:24 +0100

    Studying ncurses library....
    Signed-off-by: Massimiliano Fardo <48696348+LoscoX@users.noreply.github.com>

--- ncurses_test.cpp
+++ ncurses_test.cpp
@@ -3,29 +3,35 @@
 //
 
 #include <ncursesw/ncurses.h>
+#include "map_class.h"
+
+using namespace  std;
 
 int main ()
 {
 
     initscr();
 
-    int x,y;
-    x=y=0;
+   int height, width, start_y, start_x;
+    height = 10;
+    width = 20;
+    start_y = start_x = 10;
 
-    move(y,x);
-
-    printw("Hello world!");
-
-
+    WINDOW * win = newwin(height, width, start_y, start_x);
     refresh();
+
+    start_color();
+    init_pair(5, COLOR_GREEN, COLOR_MAGENTA);
+    box(win, 5, 58);
+    attron(COLOR_PAIR(5));
+    printw( "This Is SPARTA!");
+    attroff(COLOR_PAIR(5));
+    wrefresh(win);
+
 
     int c = getch();
 
-    printw("%d", c);
-
-    clear();
-    getch();
-
     endwin();
+
     return 0;
 }
