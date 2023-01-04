cdb4e9621720d89824f8bc216e67684f9b892bc7
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-01-04 15:10:52 +0100

    base map structure

---
+++ Map.hpp
@@ -0,0 +1,19 @@
+class Map{
+    int dim_x, dim_y;
+    Enemy enemies[];
+    Segment segments[];
+    int trigger_start, trigger_end;
+
+    Map()
+    Map(...){
+        for i in range n
+            random segment
+        for i in range n
+            random enemy
+        
+    }
+    bool isSolid(int x, int y)
+    int getX()
+    int getY()
+    toString()
+}
