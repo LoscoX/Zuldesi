f025f2df6b597e8ba97ded476aaeaa6e0efc27f6
Massimiliano Fardo <48696348+LoscoX@users.noreply.github.com>
2023-01-10 10:58:06 +0100

    Fixed thelast10prime function.
    Signed-off-by: Massimiliano Fardo <48696348+LoscoX@users.noreply.github.com>

--- Primenumebers.cpp
+++ Primenumebers.cpp
@@ -62,7 +62,7 @@ int* thelast10prime(int n){
     int* arr = new int[10];
     int y;
     if (countPrime(n)<10){
-        y = n -1;
+        y = 2;
         for (int i = 0; i<10; i++){
             if (y<=n){
                 arr[i] = nextPrime(y);
